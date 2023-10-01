f31b385843f582aa467f93a6646b0727b43f7acb
shay0454 <108744131+shay0454@users.noreply.github.com>
2023-10-01 14:15:30 +0900

    make function for escape

--- boy_following_hand.py
+++ boy_following_hand.py
@@ -8,10 +8,19 @@ open_canvas(TUK_WIDTH,TUK_HEIGHT)
 hand=load_image("hand_arrow.png")
 ch=load_image("animation_sheet.png")
 TUK_GOUND=load_image("TUK_GOUND.png")
+x,y=TUK_WIDTH//2,TUK_HEIGHT//2
+running=True
+
 def handle_events():
+    global running
     events=get_events()
-x,y
-while(1):
+    for event in events:
+        if event.type==SDL_KEYDOWN:
+            if event.key==SDLK_ESCAPE:
+                running=False
+            
+
+while(running):
     clear_canvas()
     TUK_GOUND.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
 
